be576bc41f39825d5696d0389bd6729a71cd445f
Breadleaf <bkhnapa@gmail.com>
2025-03-23 16:38:34 -0600

    added templates to update redis on startup in backend/main.go, added a redis image to docker-compose.yaml, made a redis config file that is currently unused

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -18,7 +23,105 @@ type Payload struct {
 	People []Person `json:"people"`
 }
 
+type Redis struct {
+	Connection net.Conn
+}
+
+func NewRedis(socket string) *Redis {
+	conn, err := net.Dial("tcp", socket)
+	if err != nil {
+		panic(err)
+	}
+	return &Redis{Connection: conn}
+}
+
+func (r *Redis) Close() {
+	if err := r.Connection.Close(); err != nil {
+		log.Printf("Error closing Redis connection: %v", err)
+	}
+}
+
+func (r *Redis) parseReturn() (string, error) {
+	reader := bufio.NewReader(r.Connection)
+	header, err := reader.ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+
+	switch header[0] {
+	case '+': // simple string
+		return strings.TrimSuffix(header[1:], "\r\n"), nil
+
+	case '-': // error
+		return "", errors.New(
+			strings.TrimSuffix(header[1:], "\r\n"),
+		)
+
+	case '$': // bulk string
+		var length int
+		if _, err := fmt.Sscanf(header, "$%d\r\n", &length); err != nil {
+			return "", err
+		}
+		if length == -1 {
+			return "", nil // nil value in redis
+		}
+
+		buf := make([]byte, length)
+		if _, err := reader.Read(buf); err != nil {
+			return "", err
+		}
+		// discard trailing "\r\n"
+		if _, err := reader.Discard(2); err != nil {
+			return "", nil
+		}
+
+		return string(buf), nil
+
+	default:  // not yet implemented reply
+		return "", fmt.Errorf("unsupported reply: %s", header)
+	}
+
+
+}
+
+func (r *Redis) Set(key, value string) (string, error) {
+	fmt.Fprintf(
+		r.Connection,
+		"*3\r\n$3\r\nSET\r\n$%d\r\n%s\r\n$%d\r\n%s\r\n",
+		len(key),
+		key,
+		len(value),
+		value,
+	)
+	return r.parseReturn()
+}
+
+func (r *Redis) Get(key string) (string, error) {
+	fmt.Fprintf(
+		r.Connection,
+		"*2\r\n$3\r\nGET\r\n$%d\r\n%s\r\n",
+		len(key),
+		key,
+	)
+	return r.parseReturn()
+}
+
 func main() {
+	redis := NewRedis("redis:6379")
+	defer redis.Close()
+
+	if val, err := redis.Set("name", "name@gmail.com"); err != nil {
+		fmt.Print(err)
+	} else {
+		fmt.Println("SET reply:", val)
+	}
+
+	if val, err := redis.Get("name"); err != nil {
+		fmt.Print(err)
+	} else {
+		fmt.Println("GET reply:", val)
+	}
+
 	http.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
 		log.Printf(
 			"Received request: %s %s from %s",
